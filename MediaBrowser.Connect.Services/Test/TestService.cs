d0a3bbd4371f9175ffa8b7b59e82ac395d4ec58f
Luke Pulverenti <luke.pulverenti@gmail.com>
2014-08-28 18:19:45 -0400

    added a dummy endpoint for testing purposes

---
+++ MediaBrowser.Connect.Services/Test/TestService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ServiceStack;
+
+namespace MediaBrowser.Connect.Services.Test
+{
+    [Route("/test", "GET")]
+    public class TestRequest
+    {
+        
+    }
+
+    public class TestService : Service
+    {
+        public object Get(TestRequest request)
+        {
+            return "Ok";
+        }
+    }
+}
